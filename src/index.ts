f15948197da4ef8f61389e4b1621c8df09888347
Okpube victor <curiousv32@gmail.com>
2024-08-17 07:26:44 -0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import MyUserRoute from "./routes/MyUserRoute";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/my/user", MyUserRoute);
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
+
+app.get("/test", async (req: Request, res: Response) => {
+    res.send({ message: "test OK!" });
+  });
+
+app.listen(7000, () => {
+  console.log("server started on localhost:7000");
+});
